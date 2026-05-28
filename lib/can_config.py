c84ee3082878df9b96332126f79160c9d8432cb0
Ran_cao <ran.cao@weifu.com.cn>
2026-05-28 10:23:50 +0800

    在README中新增多个UDS诊断操作的示例日志和补充说明文件

--- lib/can_config.py
+++ lib/can_config.py
@@ -3,7 +3,12 @@ import os
 import can
 
 def check_can_interfaces():
-    """扫描所有可用的 Kvaser CAN 硬件接口，过滤掉虚拟设备"""
+    """
+    扫描所有可用的 Kvaser CAN 硬件接口，过滤掉虚拟设备
+    返回一个列表，每个元素是一个字符串，格式为 "通道号: 设备名称 (SN: 序列号)"
+
+    :return: 列表，包含可用的 Kvaser CAN 硬件接口信息
+    """
     try:
         old_stderr = sys.stderr
         sys.stderr = open(os.devnull, 'w')
