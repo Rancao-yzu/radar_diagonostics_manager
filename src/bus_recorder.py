53e784bd02f313fabc38b42bf29b3d31af7e364f
Ran_cao <ran.cao@weifu.com.cn>
2026-08-21 14:10:48 +0800

    refactor(bus_recorder): 给日志写入添加线程锁
    修复多线程调用recv/send时破坏BLF日志文件的问题

--- src/bus_recorder.py
+++ src/bus_recorder.py
@@ -2,6 +2,7 @@
 """透明代理：recv/send 透传真实总线，recv 取到消息后自动写"""
 import can
 import time
+import threading
 
 
 class BusRecorder:
@@ -9,18 +10,22 @@ class BusRecorder:
     def __init__(self, bus, blf_path):
         self._bus = bus
         self._logger = can.Logger(blf_path)
+        # can.Logger (ASCWriter) 非线程安全，多线程 recv/send 并发写入会破坏文件
+        self._lock = threading.Lock()
 
     def recv(self, timeout=None):
         msg = self._bus.recv(timeout=timeout)
         if msg is not None:
-            msg.is_rx = True    
-            self._logger(msg)
+            msg.is_rx = True
+            with self._lock:
+                self._logger(msg)
         return msg
 
     def send(self, msg, timeout=None):
-        msg.is_rx = False     
-        msg.timestamp = time.time()  
-        self._logger(msg)
+        msg.is_rx = False
+        msg.timestamp = time.time()
+        with self._lock:
+            self._logger(msg)
         self._bus.send(msg, timeout=timeout)
 
     def shutdown(self):
